62e1337b642ded8121ab40f561fff8d4540d2f4c
lucky99ethan <ethansawe99@gmail.com>
2024-08-12 16:12:12 +0300

    Initial state

---
+++ shopping/src/App.jsx
@@ -0,0 +1,12 @@
+ import React from 'react';
+ 
+ const App = () => {
+   return (
+     <div className="bg-black">
+        <h1 className="bg-red-600">Shopping</h1>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
