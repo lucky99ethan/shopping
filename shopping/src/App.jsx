3f179896132b4e2722a25b97d7912d2b913176a2
lucky99ethan <ethansawe99@gmail.com>
2024-08-12 17:21:28 +0300

    initials

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -1,9 +1,11 @@
  import React from 'react';
  import Navbar from './components/Navbar/Navbar';
+ import Home from './components/Home/Home';
  const App = () => {
    return (
      <div className="">
          <Navbar/>
+         <Home/>
      </div>
    )
  }
---
+++ shopping/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h1>70% off on</h1> <br/> <h1>all product sales</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
