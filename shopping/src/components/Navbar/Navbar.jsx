9e34372661b2f5f3ac78de3fd410f86a5b20c32c
lucky99ethan <ethansawe99@gmail.com>
2024-08-12 17:17:29 +0300

    initial

--- shopping/src/components/Navbar/Navbar.jsx
+++ shopping/src/components/Navbar/Navbar.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-        <div className=''>
-         <div className="">
+    <div className='bg-blue-500 text-white shadow-lg'>
+        <div className='container mx-auto p-4'>
+         <div className="flex justify-between">
             <div className="">
-                <h1></h1>
+                <h1 className="font-bold text-xl">Shopping</h1>
+            </div>
+            <div className="flex justify-between items-center">
+                <ul className="flex space-x-12">
+                    <li className="cursor-pointer">Home</li>
+                    <li className="cursor-pointer">Products</li>
+                    <li className="cursor-pointer">About</li>
+                    <li className="cursor-pointer">Contact</li>
+                </ul>
             </div>
          </div>
         </div>
-      
     </div>
   )
 }
